9f9b9c38762b9f53a05a8b276d4dad57d42855d8
yony <4yony4@gmail.com>
2026-09-15 14:22:09 +0200

    Login Completado

--- lib/LoginView.dart
+++ lib/LoginView.dart
@@ -1,17 +1,38 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class Loginview extends StatelessWidget{
+  var faInstance=FirebaseAuth.instance;
   late BuildContext miContext;
   TextEditingController userController = new TextEditingController();
   TextEditingController passwordController = new TextEditingController();
 
-  void funClickLogin(){
+  void funClickLogin() async{
     String usuario=userController.text;
     String pass=passwordController.text;
     print("---->>>>>>>> LOGIN PRESIONADO "+usuario+"   "+pass);
-    if(usuario=="Yony" && pass=="123456"){
+
+    try {
+      await faInstance.signInWithEmailAndPassword(
+          email: usuario,
+          password: pass
+      );
+      print("LOGIN BIEN!!!");
       Navigator.popAndPushNamed(miContext, "/HomeView");
+
+    } on FirebaseAuthException catch (e) {
+      print("----------------->>>>>> "+e.toString());
+      if (e.code == 'user-not-found') {
+        print('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        print('Wrong password provided for that user.');
+      }
     }
+
+
+    /*if(usuario=="Yony" && pass=="123456"){
+      Navigator.popAndPushNamed(miContext, "/HomeView");
+    }*/
     //Navigator.pushNamed(miContext, "/HomeView");
     //Navigator.popAndPushNamed(miContext, "/HomeView");
 
