afdbf4b2ffcd89f8e1d4642887cba4577f17a84d
yony <4yony4@gmail.com>
2026-09-22 12:38:45 +0200

    Prorile actualizado en BBDD

--- lib/HomeView.dart
+++ lib/HomeView.dart
@@ -1,3 +1,4 @@
+import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dam2_2627_a/DataHolder.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -15,10 +16,19 @@ class _HomeviewState extends State<Homeview> {
   late BuildContext miContext;
 
   TextEditingController nombreController=TextEditingController();
-
   String sNombre=Dataholder.instance.perfilUsuario.name!;
-
   FirebaseFirestore db=FirebaseFirestore.instance;
+
+  int _bottomNavIndex=0;
+  List<IconData> iconList=[
+    Icons.home,
+    Icons.inbox,
+    Icons.settings,
+    Icons.person
+  ];
+
+  bool _isNotificationBadgeEnable=true;
+  bool _isMessagesBadgeEnable=true;
 
   MaskTextInputFormatter maskFormatter =  MaskTextInputFormatter(
       mask: '+# (###) ###-##-##',
@@ -48,13 +58,46 @@ class _HomeviewState extends State<Homeview> {
     miContext=context;
     nombreController.text="HOLA HOLA HOLA";
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 146, 183, 123),
+        title: Text("HOMEVIEW"),
+        actions: [
+          PopupMenuButton<String>(
+            tooltip: 'Opciones',
+            onSelected: (opcion) {
+              if (opcion == 'buscar') print("BUSCAR");
+              if (opcion == 'perfil') print("PERFIL");
+              if (opcion == 'salir') funClickLogout();
+            },
+            itemBuilder: (context) => [
+              PopupMenuItem(
+                value: 'buscar',
+                child: Row(
+                  children: [Icon(Icons.search), SizedBox(width: 8), Text('Buscar')],
+                ),
+              ),
+              PopupMenuItem(
+                value: 'perfil',
+                child: Row(
+                  children: [Icon(Icons.person), SizedBox(width: 8), Text('Perfil')],
+                ),
+              ),
+              PopupMenuItem(
+                value: 'salir',
+                child: Row(
+                  children: [Icon(Icons.logout), SizedBox(width: 8), Text('Salir')],
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
       body: Container(
         //width: 200,
         color: Color.fromARGB(255, 146, 183, 123),
         child: Column(
           mainAxisAlignment: .center,
           children: [
-            Text("HOME VIEW"),
             Text("BIENVENIDO "+sNombre),
             TextField(controller: nombreController,decoration: InputDecoration(hintText: "NOMBRE"),),
             TextField(
@@ -80,7 +123,58 @@ class _HomeviewState extends State<Homeview> {
             TextButton(onPressed: funClickLogout, child: Text("Logout"))
           ],
         ),
+      ),
+      bottomNavigationBar: NavigationBar(
+        onDestinationSelected: (int index) {
+          switch (index){
+            case 0: print("HOME");
+            case 1: {
+              print("NOTIFICATION");
+              setState(() {
+                _isNotificationBadgeEnable=false;
+              });
+            }
+            case 2: {
+              print("MESSAGES");
+              setState(() {
+                _isMessagesBadgeEnable=false;
+              });
+            }
+
+          }
+          setState(() {
+            _bottomNavIndex = index;
+          });
+        },
+        indicatorColor: Colors.amber,
+        selectedIndex: _bottomNavIndex,
+        destinations: <Widget>[
+          NavigationDestination(
+            selectedIcon: Icon(Icons.home),
+            icon: Icon(Icons.home_outlined),
+            label: 'Home',
+          ),
+          NavigationDestination(
+            icon: Badge(isLabelVisible:_isNotificationBadgeEnable, child: Icon(Icons.notifications_sharp)),
+            label: 'Notifications',
+          ),
+          NavigationDestination(
+            icon: Badge(isLabelVisible:_isMessagesBadgeEnable, label: Text('2'), child: Icon(Icons.messenger_sharp)),
+            label: 'Messages',
+          ),
+        ],
       )
+
+      /*AnimatedBottomNavigationBar(
+        icons: iconList,
+        activeIndex: _bottomNavIndex,
+        gapLocation: GapLocation.center,
+        notchSmoothness: NotchSmoothness.verySmoothEdge,
+        leftCornerRadius: 32,
+        rightCornerRadius: 32,
+        onTap: (index) => setState(() => _bottomNavIndex = index),
+        //other params
+      ),*/
     );
   }
 }
